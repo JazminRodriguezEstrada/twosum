c995dfcb260dcb955c787eaca81dcca04b94d22d
JazminRodriguezEstrada <jazminrodriguezestrada1700@gmail.com>
2024-07-31 11:27:02 -0600

    Add files via upload

---
+++ two sum arr.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+int array [6] = {10, 20, 30, 40, 50, 60};
+for (int i=0; i<6; i++)
+    cout<<endl;
+cout<<"La suma de ambas posiciones es: ";
+cout<<array[0] + array[5];
+return 0;
+}
